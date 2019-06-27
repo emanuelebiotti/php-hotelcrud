ef4e63acf4bcd028062526356ee0f5220dc7d74e
emanuele <emanuele.biotti@gmail.com>
2019-06-27 20:57:38 +0200

    schema header e nav

--- index.php
+++ index.php
@@ -23,6 +23,20 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="/public/css/app.css">
   </head>
   <body>
+
+  <header>
+    <div class="container">
+      <img src="https://www.intornodesign.it/cms/wp-content/uploads/client/bulgari/Bulgari_logo-01-500x301.png" alt="Logo hotel">
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">La nostra storia</a>
+        <a href="#">Prenota</a>
+        <a href="#">Contatti</a>
+      </nav>
+    </div>
+  </header>
+
+
     <div class="container">
       <pre>
         <?php
