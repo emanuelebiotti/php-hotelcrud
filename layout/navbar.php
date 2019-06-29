aa5ca61ba7cd70122dec0d73fdcf3a49ee3c775d
emanuele <emanuele.biotti@gmail.com>
2019-06-29 19:19:46 +0200

    Il logo ora è cliccabile per tornare alla home

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,6 +1,8 @@
 <header>
   <div class="container">
-    <img src="https://www.intornodesign.it/cms/wp-content/uploads/client/bulgari/Bulgari_logo-01-500x301.png" alt="Logo hotel">
+    <a href="index.php">
+      <img src="https://www.intornodesign.it/cms/wp-content/uploads/client/bulgari/Bulgari_logo-01-500x301.png" alt="Logo hotel">
+    </a>
     <nav>
       <a href="#">Home</a>
       <a href="#">La nostra storia</a>
