7b9cc1091f79a3c1c5eb043a6980107162cdbd93
emanuele <emanuele.biotti@gmail.com>
2019-06-29 19:17:59 +0200

    aggiunta messaggio di modifica

--- edit_manager.php
+++ edit_manager.php
@@ -5,7 +5,6 @@ include 'functions.php';
 
 $conn = connect_db($server, $user, $psw, $database);
 
-
 if($conn && $conn->connect_error) {
   echo 'errore: '.$conn->connect_error;
   exit();
@@ -22,7 +21,7 @@ $room_number = intval($_POST['room_number']);
 $floor = intval($_POST['floor']);
 $beds = intval($_POST['beds']);
 
-$sql = "UPDATE stanze SET room_number = $room_number, floor = $floor, beds = $beds WHERE id = $id";
+$sql = "UPDATE stanze SET room_number = $room_number, floor = $floor, beds = $beds, updated_at = NOW() WHERE id = $id";
 $result = $conn->query($sql);
 
  ?>
@@ -34,8 +33,9 @@ include 'layout/navbar.php';
 
  ?>
 
- <div class="container">
-  <?php var_dump($result);?>
+ <div class="container text-center">
+  <?php echo "<h1>modifica effettuata</h1>";?>
+  <a class="btn btn-primary" href="index.php">Torna alla Home</a>
 </div>
 
  <?php
--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="fixed-bottom">
   <div class="container">
     <small> Made by Emanuele - Boolean Classe 5 </small>
   </div>
